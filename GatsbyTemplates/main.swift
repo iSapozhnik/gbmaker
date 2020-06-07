3a46d856da50777388a180bbb13a1ce88a6841e7
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2020-06-07 23:55:58 +0200

    Subcommands

--- GatsbyTemplates/main.swift
+++ GatsbyTemplates/main.swift
@@ -12,70 +12,76 @@ import ArgumentParser
 import ColorizeSwift
 import Cocoa
 
-struct Make: ParsableCommand {
-    static let configuration = CommandConfiguration(abstract: "Gatsby blog template maker")
+struct Gbmaker: ParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Gatsby blog template maker", subcommands: [Make.self])
+}
 
-    private static var values: String {
-        ContentType.allCases
-            .map { "\"\($0.rawValue)\"" }
-            .joined(separator: ", ")
-    }
+extension Gbmaker {
+    struct Make: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Make command")
 
-    @Option(name: .shortAndLong, help: "The title of the article.")
-    var title: String
+        private static var values: String {
+            ContentType.allCases
+                .map { "\"\($0.rawValue)\"" }
+                .joined(separator: ", ")
+        }
 
-    @Option(name: .shortAndLong, help: "Content type. Possible values: \(values)")
-    var contentType: ContentType
+        @Option(name: .shortAndLong, help: "The title of the article.")
+        var title: String
 
-    @Option(name: .shortAndLong, help: "Website path.")
-    var path: String
+        @Argument(help: "Content type. Possible values: \(values)")
+        var contentType: ContentType
 
-    func validate() throws {
-        guard !title.isEmpty else { throw ValidationError("Title should not be an empty string") }
-        guard !path.isEmpty else { throw ValidationError("Web site path should not be an empty string") }
+        @Option(name: .shortAndLong, help: "Website path.")
+        var path: String
 
-    }
+        func validate() throws {
+            guard !title.isEmpty else { throw ValidationError("Title should not be an empty string") }
+            guard !path.isEmpty else { throw ValidationError("Web site path should not be an empty string") }
 
-    mutating func run() throws {
-        let formattedFolderName = title.replacingOccurrences(of: " ", with: "-")
-        let formattedDate = DateUtility.makeDateString()
-        let folderName = contentType == .article ? formattedFolderName : formattedFolderName.lowercased()
+        }
 
-        let blogFolder = try Folder(path: path)
-        let pagesFolder = try blogFolder.createSubfolderIfNeeded(at: "/src/pages")
-        let contentFolder = try pagesFolder.createSubfolderIfNeeded(at: contentType.directoryPath)
-        let name = formattedDate.date + "---" + folderName
-        let articleFolder = try contentFolder.createSubfolderIfNeeded(withName: name)
+        mutating func run() throws {
+            let formattedFolderName = title.replacingOccurrences(of: " ", with: "-")
+            let formattedDate = DateUtility.makeDateString()
+            let folderName = contentType == .article ? formattedFolderName : formattedFolderName.lowercased()
 
-        let fileContent = FolderUtility.makeFolder(for: title, contentType: contentType, formattedDate: formattedDate, postPath: formattedFolderName)
+            let blogFolder = try Folder(path: path)
+            let pagesFolder = try blogFolder.createSubfolderIfNeeded(at: "/src/pages")
+            let contentFolder = try pagesFolder.createSubfolderIfNeeded(at: contentType.directoryPath)
+            let name = formattedDate.date + "---" + folderName
+            let articleFolder = try contentFolder.createSubfolderIfNeeded(withName: name)
 
-        let image = NSImage.init(color: .red, size: NSSize(width: 256, height: 256))
-        let thumbFile = try articleFolder.createFile(named: "thumb.png")
-        try thumbFile.write(image.png!)
-        
-        let file = try articleFolder.createFile(named: "index.md")
-        try file.write(fileContent, encoding: .utf8)
+            let fileContent = FolderUtility.makeFolder(for: title, contentType: contentType, formattedDate: formattedDate, postPath: formattedFolderName)
 
-        let output = """
+            let image = NSImage.init(color: .red, size: NSSize(width: 256, height: 256))
+            let thumbFile = try articleFolder.createFile(named: "thumb.png")
+            try thumbFile.write(image.png!)
 
-       _                     _
-      | |                   | |
-  __ _| |__  _ __ ___   __ _| | _____ _ __
- / _` | '_ \\| '_ ` _ \\ / _` | |/ / _ \\ '__|
-| (_| | |_) | | | | | | (_| |   <  __/ |
- \\__, |_.__/|_| |_| |_|\\__,_|_|\\_\\___|_|
-  __/ |
- |___/
+            let file = try articleFolder.createFile(named: "index.md")
+            try file.write(fileContent, encoding: .utf8)
 
-"""
-        print(output.foregroundColor(.orange1))
-        print("✅ The new file has been created at:\n".foregroundColor(.orange1))
-        print("\(file.path)\n\n".foregroundColor(.white))
+            let output = """
 
+           _                     _
+          | |                   | |
+      __ _| |__  _ __ ___   __ _| | _____ _ __
+     / _` | '_ \\| '_ ` _ \\ / _` | |/ / _ \\ '__|
+    | (_| | |_) | | | | | | (_| |   <  __/ |
+     \\__, |_.__/|_| |_| |_|\\__,_|_|\\_\\___|_|
+      __/ |
+     |___/
+
+    """
+            print(output.foregroundColor(.orange1))
+            print("✅ The new \(contentType.rawValue) has been created at:\n".foregroundColor(.orange1))
+            print("\(file.path)\n\n".foregroundColor(.white))
+
+        }
     }
 }
 
-Make.main()
+Gbmaker.main()
 
 extension NSBitmapImageRep {
     var png: Data? { representation(using: .png, properties: [:]) }
