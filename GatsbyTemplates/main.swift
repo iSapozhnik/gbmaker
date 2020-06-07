bdd382a9981c550515906c4d785cf83714281d44
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2020-06-07 17:18:11 +0200

    Initial Commit

---
+++ GatsbyTemplates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GatsbyTemplates
+//
+//  Created by Ivan Sapozhnik on 07.06.20.
+//  Copyright © 2020 heavylightapps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
